8322620728b8935e353223c339eb09209b2060c4
Loganlaut14 <43793380+Loganlaut14@users.noreply.github.com>
2020-01-15 13:59:30 -0800

    EnemyAi Stuffs

---
+++ Assets/Scripts/EnemyDetection.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+    private EnemyMaster enemyMaster;
+    private Transform myTransform;
+    public Transform head;
+    public LayerMask playerlayer;
+    public LayerMask sightLayer;
+    private float checkRate;
+    private float nextCheck;
+    private float detectRadius = 80;
+    private RaycastHit hit;
+
+    void OnEnable()
+    {
+        SetInitialRefrences();
+        enemyMaster.EventEnemyDie += DisableThis;
+
+    }
+
+    void OnDisable()
+    {
+        enemyMaster.EventEnemyDie -= DisableThis;
+    }
+
+    void Update()
+    {
+        CarryOutDetection();
+        
+
+    }
+
+    void SetInitialRefrences()
+    {
+        enemyMaster = GetComponent<EnemyMaster>();
+        myTransform = transform;
+
+        /*if (head = null)
+        {
+            head = myTransform;
+        }*/
+
+        checkRate = Random.Range(0.8f, 1.2f);
+    }
+
+    void CarryOutDetection()
+    {
+        if (Time.time > nextCheck)
+        {
+            nextCheck = Time.time + checkRate;
+
+            Collider[] colliders = Physics.OverlapSphere(myTransform.position, detectRadius, playerlayer);
+
+            if (colliders.Length > 0)
+            {
+                foreach (Collider potentialTargetCollider in colliders)
+                {
+                    if (potentialTargetCollider.CompareTag(GameManager_References._playerTag))
+                    {
+                        if (CanPotentialTargetBeSeen(potentialTargetCollider.transform))
+                        {
+                            break;
+                        }
+                    }
+
+                }
+            }
+
+            else
+            {
+                enemyMaster.CallEventEnemyLostTarget();
+            }
+
+              
+            }
+        }
+
+        bool CanPotentialTargetBeSeen(Transform potentialTarget)
+        {
+            if (Physics.Linecast(head.position, potentialTarget.position, out hit, sightLayer))
+            {
+                if (hit.transform == potentialTarget)
+                {
+                    enemyMaster.CallEventEnemySetNavTarget(potentialTarget);
+                    return true;
+                }
+                else
+                {
+                    enemyMaster.CallEventEnemyLostTarget();
+                    return false;
+                }
+            }
+            else
+            {
+                enemyMaster.CallEventEnemyLostTarget();
+                return false;
+            }
+        }
+
+    void DisableThis()
+    {
+        this.enabled = false;
+
+    }
+}
+
+    
+ 
+
---
+++ Assets/Scripts/Enemy_NavPursue.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_NavPursue : MonoBehaviour
+{
+    private EnemyMaster enemyMaster;
+    private UnityEngine.AI.NavMeshAgent navMeshAgent;
+    private float checkRate;
+    private float nextCheck;
+
+    void OnEnable()
+    {
+        SetInitialRefrences();
+        enemyMaster.EventEnemyDie += DisableThis;
+
+    }
+
+    void OnDisable()
+    {
+        enemyMaster.EventEnemyDie -= DisableThis;
+
+    }
+
+    void Update()
+    {
+        if (Time.time > nextCheck)
+        {
+            nextCheck = Time.time + checkRate;
+            TryToChaseTarget();
+        }
+    }
+
+    void SetInitialRefrences()
+    {
+        enemyMaster = GetComponent<EnemyMaster>();
+        if(GetComponent<UnityEngine.AI.NavMeshAgent>()!= null)
+        {
+            myNavMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        }
+        checkRate = Random.Range(0.1f, 0.2f);
+    }
+
+    void TryToChaseTarget()
+    {
+        if(enemyMaster.myTarget != null && !enemyMaster.isNavPaused)
+        {
+            myNavMeshAgent.SetDestination(enemyMaster.myTarget.position);
+
+            if(myNavMeshAgent.remainingDistance>myNavMeshAgent.stoppingDistance)
+            {
+                enemyMaster.CallEventEnemyWalking();
+                enemyMaster.isOnRoute = true;
+                
+            }
+
+        }
+    }
+
+    void DisableThis()
+    {
+        if(myNavmeshAgent != null)
+        {
+            myNavMeshAgent.enabled = false;
+        }
+
+        this.enabled = false;
+    }
+
+
+
+
+}
