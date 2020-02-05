6c2a4bfcf4eee9666b3fc305084940adbd64bca4
Maxwell Bake <20.maxwell.dickes@ksd.org>
2020-02-05 13:40:24 -0800

    script conflict

---
+++ Assets/Scripts/Mana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mana : MonoBehaviour
+{
+    public int mana = 100;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
