13c5692d939c6943b0fb790e377fc1989df36d72
Ch3wbacca <43761466+Ch3wbacca@users.noreply.github.com>
2020-02-11 11:51:04 -0800

    worked in enemystuff

---
+++ Assets/Trooper/TrooperShoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrooperShoot : MonoBehaviour
+{
+    public bool shoot;
+    public Transform BarrelLocation;
+    public AudioSource bang;
+    void Start()
+    {
+        if (BarrelLocation == null)
+            BarrelLocation = transform;
+        
+    }
+    public void TriggerShoot()
+    {
+        GetComponent<Animator>().SetBool("Fire", true);
+
+    }
+    public void stopShoot()
+    {
+        GetComponent<Animator>().SetBool("Fire", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
