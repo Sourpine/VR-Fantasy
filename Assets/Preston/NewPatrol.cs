f9e48773d35ae98052b7a0237ebeb99d4f78d48f
Ch3wbacca <43761466+Ch3wbacca@users.noreply.github.com>
2020-02-19 13:09:43 -0800

    working on patrol for ai

---
+++ Assets/Preston/NewPatrol.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class NewPatrol : MonoBehaviour
+{
+    [SerializeField]
+    bool _patrolWaiting;
+
+    [SerializeField]
+    float _totalWaitTime = 3f;
+
+    [SerializeField]
+    float _switchProbability = 0.2f;
+
+    [SerializeField]
+    List<Waypoint> _patrolPoints;
+
+    NavMeshAgent _navMeshAgent;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Preston/Patrol.cs
+++ Assets/Preston/Patrol.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Patrol : MonoBehaviour
 {
     public float speed;
-    public Transform[] moveSpots;
+    public Transform moveSpots;
     private int randomSpot;
-
+    public float startWaitTime;
+    private float WaitTime;
+    public float maxX;
+    public float minX;
+    public float minY;
+    public float maxY;
+    public Animator bruh;
      void Start()
     {
-        randomSpot = Random.Range(0, moveSpots.Length);
-            
+        WaitTime = startWaitTime;
+        moveSpots.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        
     }
     private void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots.position, speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, moveSpots.position) < 0.2f)
+        {
+            if(WaitTime <= 0)
+            {
+                moveSpots.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                WaitTime = startWaitTime;
+                bruh.SetBool("Waiting?", false);
+            }
+            else
+            {
+                WaitTime -= Time.deltaTime;
+                bruh.SetBool("Waiting?", true);
+            }
+        }
     }
 }
