505ab6dfcc820bbd6ecae429c86462a180101aaf
John Kaser <20.john.kaser@ksd.org>
2020-02-24 13:22:05 -0800

    robofire

---
+++ Assets/LucasAssets/BossShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform barrelLocation;
+    public float shotPower = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Fire()
+    {
+        Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+    }
+}
