bb8a9c91bda815e0e96919308ad3456a58f47878
John Kaser <20.john.kaser@ksd.org>
2020-02-27 11:55:36 -0800

    bruh

---
+++ Assets/LucasAssets/Scripts/AoeDiss.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AoeDiss : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Dissipate());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Dissipate()
+    {
+        yield return new WaitForSeconds(2.5f);
+        Destroy(gameObject);
+    }
+}
