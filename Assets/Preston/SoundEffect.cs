f45372ff3fd3ffee8dfec61af8881f7c8d710bbe
Ch3wbacca <43761466+Ch3wbacca@users.noreply.github.com>
2020-03-05 12:49:17 -0800

    JohnCity

---
+++ Assets/Preston/SoundEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour
+{
+    public AudioSource Bruh;
+    public AudioClip Footstep;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Sound()
+    {
+        Bruh.PlayOneShot(Footstep);
+    }
+}
